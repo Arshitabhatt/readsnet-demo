6df2c0b8173de91890a6ce818bcc431ef316a8b7
Arshita Bhatt <aarshibhatt112@gmail.com>
2019-09-08 22:17:54 +0530

    errors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const express = require("express");
 
       const db = require('./config/key').mongoURI;
 
-      mongoose.connect(db,{
+      mongoose.connect(process.env.DATABASEURL,{
         useNewUrlParser: true,
         useCreateIndex: true
     }).then(()=>{
@@ -104,7 +104,7 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'readsnet-front', 'build', 'index.html'))
     })
 }
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log('http://localhost:5000')
 });
 
